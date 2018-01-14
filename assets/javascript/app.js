07fa2363db1ba39ed915972353bad277f5a02902
Zach Johnson <zachtjohnson01@gmail.com>
2018-01-13 18:02:00 -0600

    Added presence tracker

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,3 +12,103 @@
 //Create a variable to reference the database
 
 var database = firebase.database();
+
+// Store connections in variable
+var connectionsRef = database.ref("/connections");
+
+// '.info/connected' is boolean value provided by Firebase
+// updates when client's connection state changes
+var connectedRef = database.ref(".info/connected");
+
+// Store players in variable
+var playersList = database.ref("/playersList");
+
+
+
+
+// When the client's connection state changes...
+connectedRef.on("value", function(snap){
+  
+  // If they are connected..
+  if (snap.val()) {
+    var playersCount = 0;
+    playersCount ++;
+    console.log(playersCount);
+    
+    
+    // Add user to connections list
+    var con = connectionsRef.push(`player${playersCount}`);
+    
+    // Remove user form the connection list when they disconnect
+    con.onDisconnect().remove();
+  }
+});
+
+// database.ref().on("value", function(snapshot) {
+
+// });
+
+// $(".rock_p1").on("click", function() {
+//   database.ref().set( {
+//     playerMove: "rock"
+//   });
+//   console.log("rock");
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const messaging = firebase.messaging();
+// messaging.requestPermission()
+// .then(function() {
+//   console.log('Have permission');
+//   return messaging.getToken();
+// })
+// .then(function(token) {
+//   console.log(token);
+// })
+// .catch(function(err) {
+//   console.log('Error occured');
+// })
+
+
+
+
+// console.log(config.databaseURL);
+
+
+
+// var amOnline = new Firebase(`https://${config.databaseURL}.firebaseio.com/.info/connected`);
+// var userRef = new Firebase(`https://${config.databaseURL}.firebaseio.com/presence/${userId}`);
+// amOnline.on('value', function(snapshot) {
+//   if (snapshot.val()) {
+//     userRef.onDisconnect().remove();
+//     userRef.set(true);
+//   }
+// });
